7f827d4dec5db186fd4235e79c0260fc4f1fd2b0
Yoni <firroloni.yoni@gmail.com>
2021-04-13 12:47:18 +0200

    rooting test

---
+++ pages/admin/tracks.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export default function Board()
+ {
+
+  const [isAuth, setAuth] = useState(false)
+
+  useEffect(() => {
+    axios.get('/').
+    then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  })
+  console.log(isAuth)
+   return (
+     <>
+     <Head>
+       Administration panel
+     </Head>
+     Here manage your tracks
+     </>
+   )
+ }
+
+
+
